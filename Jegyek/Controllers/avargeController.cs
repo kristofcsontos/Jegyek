5553c0e88779a9e174cbb8af1122dcba0a5042af
kristofcsontos <csontoskristof@gmail.com>
2023-01-05 07:20:20 +0100

    Átlagok kiszámítása

--- Jegyek/Controllers/avargeController.cs
+++ Jegyek/Controllers/avargeController.cs
@@ -1,6 +1,7 @@
 ﻿using Jegyek.Data;
 using Jegyek.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Jegyek.Controllers
 {
@@ -16,17 +17,46 @@ namespace Jegyek.Controllers
         // GET: jegyekInfoes
         public async Task<IActionResult> Avarge()
         {
-            var courses =_context.Courses.ToList();
-            
-
-            // összegzés a jegyek átlagának kiszámításához
+            var courses = _context.Courses.ToList();
+            double creditIndex = 0;
+            int semesterMax = courses.Max(c => c.Grade);
+            double weightedAverage = 0;
             double totalGrade = 0;
+            int curledCreditIndex = 0; //Göngyölt kredit index
+            int curledGrade = 0;  //Göngyölt jegyek
+            int sumKredit = 0; 
+            int semesterKredit = 0; 
+
             foreach (var course in courses)
             {
-                totalGrade += course.Grade;
+                if (semesterMax == course.Semester)
+                {
+                    //átlag
+                    totalGrade = totalGrade + course.Grade;
+                    //Kredit index
+                    creditIndex = creditIndex + (course.Credit * course.Grade);
+                    //Súlyozott tanulmányi átlag
+                    weightedAverage = course.Credit + weightedAverage;
+                    //Teljesített kredit
+                    semesterKredit = semesterKredit + course.Credit;
+                }
+                curledCreditIndex = curledCreditIndex + (course.Credit * course.Grade);
+                curledGrade = curledGrade + course.Grade;
+                sumKredit = sumKredit + course.Credit;
             }
+            ViewBag.CreditIndex = creditIndex / 30;
+
+            weightedAverage = Convert.ToInt32(creditIndex) / weightedAverage;
+            ViewBag.WeightedAvarge = weightedAverage;
+
+            ViewBag.CurledAverage = Convert.ToDouble(curledCreditIndex) / Convert.ToDouble(curledGrade);
+
+            ViewBag.SumKredit = sumKredit;
+
+            ViewBag.SemesterKredit = semesterKredit;
+
+            ViewBag.a = semesterMax;
             double atlag = totalGrade / courses.Count;
-            //ViewBag.Average = atlag;
             return View(atlag);
         }
     }
